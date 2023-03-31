88e77f944093f2c1898272f780bc0a84c110d7e5
Joalor64 <rhgarcon@gmail.com>
2023-03-31 07:46:07 -0400

    mleb

--- source/GithubShit.hx
+++ source/GithubShit.hx
@@ -3,7 +3,7 @@ package;
 class GithubShit
 {
     public static var repoHolder:String = 'Joalor64GH';
-    public static var repoName:String = 'ChainSaw-Engine-Personalized';
+    public static var repoName:String = 'Quasar-Engine';
     public static var personalAccessToken:String = ''; // unnecessary thingy i guess
 }
  // CHANGE THIS IF YOU ARE MAKING A MOD!!!!!!!!!!!!!!!!!!
